cfbb59a1925f5fa78f786b4f683e7fe86ce4d0f4
Tosin Akerele <akerele.james1699@gmail.com>
2023-07-10 06:55:35 +0100

    feat: did 2 steps modal

--- src/pages/all_banks/index.jsx
+++ src/pages/all_banks/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import React, { useState } from "react";
 // import data from "../../utils/data/banks.json";
 import "./banks.css";
 import { useEffect } from "react";
@@ -12,14 +12,16 @@ const AllBanks = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
-  const [isModalClicked, setIsModalClicked] = useState(false);
+  const [isModalClicked, setIsModalClicked] = useState({
+    state: false,
+    name: "",
+  });
 
-  document.addEventListener('keydown', (e)=>{
-    
-    if(e.key==='Escape') {
-      setIsModalClicked(false)
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      setIsModalClicked({ state: false, name: "" });
     }
-  })
+  });
 
   const url = "https://nigerianbanks.xyz/";
 
@@ -47,6 +49,7 @@ const AllBanks = () => {
 
   useEffect(() => {
     fetchBanks();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [bankData.length]);
 
   return (
@@ -62,14 +65,14 @@ const AllBanks = () => {
       ) : null}
 
       {!loading ? (
-        bankData.map((bank, idx) => {
+        bankData.map((bank) => {
           let { slug, name, logo } = bank;
-          idx = slug;
+          // idx = slug;
           return (
             <div key={slug} className="">
               <div
                 className="cursor-pointer relative single_bank bg-white dark:bg-slate-800 p-8 flex flex-col items-center justify-center after:ease-linear after:duration-300"
-                onClick={() => setIsModalClicked(true)}
+                onClick={() => setIsModalClicked({ state: true, name })}
               >
                 <img src={logo} alt={name} className="" />
               </div>
@@ -84,52 +87,109 @@ const AllBanks = () => {
           Loading...
         </div>
       )}
-      <Modal showModal={isModalClicked} setShowModal={setIsModalClicked}/>
+      <Modal showModal={isModalClicked} setShowModal={setIsModalClicked} />
     </div>
   );
 };
 
 export default AllBanks;
 
-const Modal = ({showModal,setShowModal}) => {
+const Modal = ({ showModal, setShowModal }) => {
+  const [step1, setStep1] = useState(false);
 
   return (
     <div
-      className={`absolute h-screen w-screen bg-black/40 overflow-hidden top-0 bottom-0 z-50 left-0 right-0 flex items-center justify-center modal dark:bg-grey/70 ${!showModal && 'hidden'}`}
+      className={`fixed h-screen w-screen bg-black/40 overflow-hidden top-0 bottom-0 z-50 left-0 right-0 flex items-center justify-center modal dark:bg-grey/70 ${
+        !showModal.state && "hidden"
+      }`}
     >
       <form
-        className="bg-white dark:bg-grey p-8 rounded w-8/12 md:w-5/12 xl:w-3/12 space-y-3 relative"
+        className="bg-white dark:bg-grey p-8 rounded w-8/12 md:w-5/12 xl:w-3/12space-y-3  relative"
         onSubmit={(e) => e.preventDefault()}
       >
-        <span className="absolute top-0 right-0 duration-200 ease-in rounded-se cursor-pointer hover:bg-red-700 px-2 py-1 hover:text-white " onClick={() => setShowModal(false)}>&#128473;</span>
-        <div className="">
-          <InputField
-            type="email"
-            labelName="User ID"
-            placeholder="Enter your user id..."
-            color="primary"
-            htmlFor="user_id"
+        <span
+          className="absolute top-0 right-0 duration-200 ease-in rounded-se cursor-pointer hover:bg-red-700 px-2 py-1 hover:text-white "
+          onClick={() => setShowModal({ state: false, name: "" })}
+        >
+          &#128473;
+        </span>
+
+        {/* step 1 form */}
+        <div className={`space-y-3 ${step1 && "hidden"}`}>
+          <div className="text-center font-semibold pb-2 text-lg">
+            Choose Account Type.
+          </div>
+          <input
+            type="radio"
+            name="account_type"
+            id="type1"
+            className="mr-2"
+            value="personal"
+            required
           />
-        </div>
-        <div className="">
-          <InputField
-            type="password"
-            placeholder="Enter your password"
-            labelName="Password"
-            color="primary"
-            htmlFor="password"
+          <label htmlFor="type1">Personal Account</label>
+          <br />
+          <input
+            type="radio"
+            name="account_type"
+            id="type2"
+            className="mr-2"
+            value="company"
+            required
           />
+          <label htmlFor="type2">Company Account</label>
+          <br />
+          <input type="checkbox" className="mr-2 mt-4" id="add_account" />
+          <label htmlFor="add_account" className="mt-4 font-medium">
+            Add {showModal.name ?? "--"}
+          </label>
+          <div className="flex items-center justify-center gap-3">
+            <button
+              className="text-[#6366F1] bg-white border border-[#6366F1] font-semibold w-1/3 md:w-2/6 hover:shadow-lg py-2 px-3 rounded flex items-center justify-center text-xs sm:text-base mt-4"
+              onClick={() => {setShowModal({state:false,name:''}); setStep1(false)}}
+            >
+              Cancel
+            </button>
+            <button
+              className="bg-[#6366F1] text-white border font-semibold w-1/3 md:w-2/6 hover:shadow-lg py-2 px-3 rounded flex items-center justify-center text-xs sm:text-base mt-4"
+              onClick={() => setStep1(true)}
+            >
+              Next
+            </button>
+          </div>
         </div>
-        <div className="flex items-center justify-center gap-3">
-          <button
-            className="text-[#6366F1] bg-white border border-[#6366F1] font-semibold w-1/3 md:w-2/6 hover:shadow-lg py-2 px-3 rounded flex items-center justify-center text-xs sm:text-base mt-4"
-            onClick={() => setShowModal(false)}
-          >
-            Cancel
-          </button>
-          <button className="bg-[#6366F1] text-white border font-semibold w-1/3 md:w-2/6 hover:shadow-lg py-2 px-3 rounded flex items-center justify-center text-xs sm:text-base mt-4">
-            Submit
-          </button>
+
+        {/* step 2 form */}
+        <div className={`space-y-3 ${!step1 && "hidden"}`}>
+          <div className="">
+            <InputField
+              type="email"
+              labelName="User ID"
+              placeholder="Enter your user id..."
+              color="primary"
+              htmlFor="user_id"
+            />
+          </div>
+          <div className="">
+            <InputField
+              type="password"
+              placeholder="Enter your password"
+              labelName="Password"
+              color="primary"
+              htmlFor="password"
+            />
+          </div>
+          <div className="flex items-center justify-center gap-3">
+            <button
+              className="text-[#6366F1] bg-white border border-[#6366F1] font-semibold w-1/3 md:w-2/6 hover:shadow-lg py-2 px-3 rounded flex items-center justify-center text-xs sm:text-base mt-4"
+              onClick={() => setStep1(false)}
+            >
+              Previous
+            </button>
+            <button className="bg-[#6366F1] text-white border font-semibold w-1/3 md:w-2/6 hover:shadow-lg py-2 px-3 rounded flex items-center justify-center text-xs sm:text-base mt-4">
+              Submit
+            </button>
+          </div>
         </div>
       </form>
     </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div className="absolute top-1/2 -translate-y-1/2 uppercase text-4xl sm:text-5xl md:text-6xl lg:text-7 xl:text-8xl  dark:text-indigo-100 text-center font-extrabold p-2 duration-300 ease-linear">
-      Manage all your bank accounts in one place.{" "}
+    <div className="absolute top-1/2 -translate-y-1/2 duration-300 ease-linear flex justify-center flex-col items-center">
+      <h2 className="uppercase mb-6 text-4xl sm:text-5xl md:text-6xl lg:text-7 xl:text-8xl dark:text-indigo-100 text-center font-extrabold p-2 ">
+        Manage all your bank accounts in one place
+      </h2>
+
+      <Link
+        to="/login"
+        className="text-center bg-primary rounded-md text-white px-8 py-4 text-lg font-semibold"
+      >
+        Get Started
+      </Link>
     </div>
   );
 };
