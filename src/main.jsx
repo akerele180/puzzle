1a918d6f7b9ac69a6ea3851b5a4eb79ba6199091
Tosin Akerele <akerele.james1699@gmail.com>
2023-07-13 04:44:16 +0100

    feat: added new font face.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './assets/fonts/milik/Millik.ttf'
+import './assets/fonts/Moderat/Moderat-Regular.ttf'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
